e8cbe16a0faff0233dd715e7126a2568e48a0003
wiro <wiro@livesport.eu>
2018-07-18 19:48:46 +0200

    Racktables api distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+    name="racktables-api",
+    version="0.2.1",
+    packages=["rtapi"],
+    license="GPLv2",
+    description="Simple racktables API",
+    url="https://github.com/rvojcik/rtapi",
+    author="Robert Vojcik",
+    author_email="robert@vojcik.net",
+    keywords=['rtapi', 'racktables', 'racktables api'],
+    classifiers=[
+            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+    ]
+)
+
+
