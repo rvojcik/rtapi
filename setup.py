00133e38d69e5aec95de29e2faffb5eff77330f0
Robert Vojcik <robert@vojcik.net>
2018-07-19 09:41:16 +0200

    New release, fixed up distribution pypi keywords

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name="racktables-api",
-    version="0.2.1",
+    version="0.2.2",
     packages=["rtapi"],
     license="GPLv2",
     description="Simple racktables API",
     url="https://github.com/rvojcik/rtapi",
     author="Robert Vojcik",
     author_email="robert@vojcik.net",
-    keywords=['rtapi', 'racktables', 'racktables api'],
+    keywords=['rtapi', 'racktables', 'racktables api', 'racktables-api','racktables cli','racktables-cli'],
     classifiers=[
             "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
             "Operating System :: POSIX",
